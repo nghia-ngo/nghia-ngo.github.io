904d28f8cb918223a97d1ddb14bb1f6e901c2b53
Nghia Ngo <nghiant6554@co-well.com.vn>
2018-07-17 09:53:05 +0700

    Lazy load images

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,6 +21,14 @@
 
         // });
 
+        // Lazy load
+        var allimages= document.getElementsByTagName('img');
+        for (var i=0; i<allimages.length; i++) {
+            if (allimages[i].getAttribute('data-src')) {
+                allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+            }
+        }
+
     });
 
     /*---------------------------------------------------
